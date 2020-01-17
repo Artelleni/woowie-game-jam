d44f8a6b60d9fb2533279b1aa17281698c78fcf9
david <davidpedro.chico@gmail.com>
2020-01-17 14:44:26 +0100

    Basic console controls

---
+++ Content/Console/Label.gd
@@ -0,0 +1,68 @@
+extends Node2D
+
+var inputstr = "" setget _set_inputstr
+
+var writing = false setget _set_writing
+
+
+var history = []
+var index = 0
+
+var commands = []
+
+
+func _ready():
+	self.writing = false
+	self.inputstr = ""
+
+func _input(event):
+	if event is InputEvent and event.is_pressed():
+		if writing:
+			if event.is_action_pressed("ui_exit"):
+				self.writing = false
+			else:
+				var s = event.as_text()
+				
+				if s == "Enter":
+					connect("commandInput", self, "_command_entered", [inputstr])
+					history.append(inputstr)
+					self.inputstr = ""
+					self.writing = false
+				if s == "BackSpace":
+					self.inputstr = inputstr.substr(0, inputstr.length() - 1)
+				if s == "Space":
+					self.inputstr = self.inputstr + " "
+				if s == "Minus":
+					self.inputstr = self.inputstr + "-"
+				if s == "Shift+7":
+					self.inputstr = self.inputstr + "/"
+				
+				self.inputstr = self.inputstr + s
+#
+#				if s.length() == 1:
+#					self.inputstr = self.inputstr + s.to_lower()
+		elif event.is_action_pressed("ui_write"):
+			self.writing = true
+
+func historyApend():
+	pass
+	
+func historyUp():
+	pass
+	
+func historyDown():
+	pass
+
+
+func _set_inputstr(text):
+	inputstr = text
+	$GUILayer/TextInputDisplay.set_text(inputstr)
+	
+func _set_writing(val):
+	writing = val
+	self.inputstr = ""
+	
+	if writing:
+		$GUILayer/WritingLabel.visible = true
+	else:
+		$GUILayer/WritingLabel.visible = false
